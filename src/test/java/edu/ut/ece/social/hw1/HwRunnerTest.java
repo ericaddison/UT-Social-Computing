6426e7b5a35cc5c0a9375b08caf549566ca7f5d7
abruck1 <agbruck@gmail.com>
2018-09-04 23:31:13 -0500

    Added the sample test cases from TA as well as some junit tests for them. We should come up with a way to sort according to leftSideNodes and convert the negative rightSideNodes to positive to match the expected outputs

--- src/test/java/edu/ut/ece/social/hw1/HwRunnerTest.java
+++ src/test/java/edu/ut/ece/social/hw1/HwRunnerTest.java
@@ -12,6 +12,7 @@ import org.junit.runners.JUnit4;
 
 import java.io.*;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 import static com.google.common.truth.Truth.assertThat;
@@ -51,6 +52,73 @@ public class HwRunnerTest {
         assertThat(testOutputArray[3]).contains("(2,-2)");
         assertThat(testOutputArray[4]).contains("(3,-3)");
         assertThat(testOutputArray).hasLength(5);
+    }
+
+    @Test
+    public void justRunItDGSTest1_shouldNotThrowAnyExceptions() throws FileNotFoundException {
+        String testOutput = HwRunner.runMaximumMatchingProblemWithOutput("./src/test/resources/test1.txt", DGS::maxMatchingDGS);
+
+        String[] testOutputArray = testOutput.split("\n");
+        assertThat(testOutputArray[0]).matches("Elapsed time:.*ms");
+        assertThat(testOutputArray[1]).contains("449");
+        assertThat(testOutputArray[3]).contains("(1,-2)");
+        assertThat(testOutputArray[5]).contains("(2,-4)");
+        assertThat(testOutputArray[4]).contains("(3,-3)");
+        assertThat(testOutputArray[2]).contains("(4,-1)");
+        assertThat(testOutputArray[6]).contains("(5,-5)");
+        assertThat(testOutputArray).hasLength(7);
+    }
+
+    @Test
+    public void justRunItDGSTest2_shouldNotThrowAnyExceptions() throws FileNotFoundException {
+        String testOutput = HwRunner.runMaximumMatchingProblemWithOutput("./src/test/resources/test2.txt", DGS::maxMatchingDGS);
+
+        String[] testOutputArray = testOutput.split("\n");
+
+        assertThat(testOutputArray[0]).matches("Elapsed time:.*ms");
+        assertThat(testOutputArray[1]).contains("7");
+        assertThat(testOutputArray[4]).contains("(1,-3)");
+        assertThat(testOutputArray[5]).contains("(2,-4)");
+        assertThat(testOutputArray[3]).contains("(3,-2)");
+        assertThat(testOutputArray[7]).contains("(4,-6)");
+        assertThat(testOutputArray[11]).contains("(5,-10)");
+        assertThat(testOutputArray[8]).contains("(6,-7)");
+        assertThat(testOutputArray[10]).contains("(7,-9)");
+        assertThat(testOutputArray[2]).contains("(8,-1)");
+        assertThat(testOutputArray[6]).contains("(9,-5)");
+        assertThat(testOutputArray[9]).contains("(10,-8)");
+        assertThat(testOutputArray).hasLength(12);
+    }
+
+    @Test
+    public void justRunItDGSTest3_shouldNotThrowAnyExceptions() throws FileNotFoundException {
+        String testOutput = HwRunner.runMaximumMatchingProblemWithOutput("./src/test/resources/test3.txt", DGS::maxMatchingDGS);
+
+        String[] testOutputArray = testOutput.split("\n");
+
+        assertThat(testOutputArray[0]).matches("Elapsed time:.*ms");
+        assertThat(testOutputArray[1]).contains("17889");
+    }
+
+    @Test
+    public void justRunItDGSTest4_shouldNotThrowAnyExceptions() throws FileNotFoundException {
+        String testOutput = HwRunner.runMaximumMatchingProblemWithOutput("./src/test/resources/test4.txt", DGS::maxMatchingDGS);
+
+        String[] testOutputArray = testOutput.split("\n");
+
+        assertThat(testOutputArray[0]).matches("Elapsed time:.*ms");
+        assertThat(testOutputArray[1]).contains("473");
+    }
+
+    @Test
+    public void justRunItDGSTestM3_shouldNotThrowAnyExceptions() throws FileNotFoundException {
+        String testOutput = HwRunner.runMaximumMatchingProblemWithOutput("./src/test/resources/M3.txt", DGS::maxMatchingDGS);
+
+        String[] testOutputArray = testOutput.split("\n");
+
+        assertThat(testOutputArray[0]).matches("Elapsed time:.*ms");
+        assertThat(testOutputArray[1]).contains("94");
+
     }
 
     private static class CachingPrintStream extends PrintStream {
